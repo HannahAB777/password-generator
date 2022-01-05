14a7e806a3973864f489a0364a42eea56e0c0e59
hannah AB <hannah.allisonbatt@gmail.com>
2022-01-05 13:21:40 +0800

    updated  html and javascript to reflect  readme

--- assets/script.js
+++ assets/script.js
@@ -1,78 +1,76 @@
-// when button is clicked a password in generated
-
-const generateBtn = document.getElementById("#generate");
-const passwordrange = document.getElementById("#length");
-const uppercaseInput = document.getElementById("#uppercase");
-const lowercaseInput = document.getElementById("#lowercase");
-const numberInput = document.getElementById ("#numbers");
-const symbolInput = document.getElementById ("#symbols");
-
-const generateBtn = document.getElementById("#generate");
-
-const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowercase = "abcdefghijklmnopqrstuvwxyz";
-const number = "0123456789";
-const symbol = "!@#$%^&*(?";
-
-// Add event listener to generate button
-generateBtn.addEventListener("click",function (event){
-
-
-    const passwordlength = Number(passwordrange.value);
-    const hasUppercase = uppercaseInput.checked;
-    const hasLowercase = lowercaseInput.checked;
-    const hasNumber = numberInput.checked;
-    const hasSymbol = symbolInput.checked;
-
-    //if no boxes are checked
-    const noChoiceMade = !hasUppercase && !hasLowercase && !hasNumber && !hasSymbol;
-
-    if(noChoiceMade){
-        //const user know they must selected atleast one box
-        alert("Please check at least one box.");
-        return;
+//confirm number of characters
+var numberOfCharacters = window.prompt('how many characters would you like to add', 'type a number between 8-128');
+    //if not a number or outside of 8-128 return an error message
+    if (isNaN(numberOfCharacters)){
+        alert('invalid selection, please enter a number');
+        window.prompt('how many characters would you like to add', 'type a number between 8-128');
+       
+    } if (numberOfCharacters < 8){
+        alert('invalid please enter a number between 8-128');
+        window.prompt('how many characters would you like to add', 'type a number between 8-128');
     }
-
-    let characterset = "";
-
-    if(hasUppercase){
-        characterset = characterset + uppercase;
+     if (numberOfCharacters > 128){
+    alert('invalid please enter a number between 8-128');
+    window.prompt('how many characters would you like to add', 'type a number between 8-128');
     }
-    if(hasLowercase){
-        characterset = characterset + lowercase;
+var addNumbers = window.confirm('would you like to add numbers?'); //to add numbers
+var addUppercase = window.confirm('would you like to add uppercase letters?'); //to add uppercase
+var addLowercase = window.confirm('would you like to add lowercase latters?'); //to add lowercase
+var addsymbols = window.confirm('would you like to add symbols?'); //to add symbols
+    //if no character types are selected return error and start code again
+    if (!addNumbers&&!addUppercase&&!addLowercase&&!addsymbols) {
+        alert('error, please select at least one character type.');
     }
-    if(hasNumber){
-        characterset = characterset + number;
-    }
-    if(hasSymbol){
-        characterset = characterset + symbol;
-    }
-
-    console.log(characterset);
-
-    for (let index = 0; index < passwordlength; index++) {
-        const element = array[index];
-        
-    }
-});
-
-
-
-//when boxes are checked
-// a password between 8-128characters
-//if user enters numbers outside of this an error is logged
-//must include numbers
-//must include uppercase
-//lowercase
-//symbols
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+//create var for gernerate password button
+var passwordBttn = document.getElementById('#generate');
+//length of password
+var passwordLength = Number(numberOfCharacters);
+//create functions for each type of character
+//numbers
+function getRandomNumber() {
+    return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
+//uppercase
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+//lowercase
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+//symbols
+function getRandomSymbol() {
+    const symbols = '!@#$%^&*(){}[]=<>/,.'
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+//passwordoutput
+var characterset = "";
+if (!!addNumbers){
+   characterset = characterset + getRandomNumber();
+}
+if (!!addUppercase){
+    characterset = characterset + getRandomUpper();
+ }
+ if (!!addLowercase){
+    characterset = characterset + getRandomLower();
+ }
+ if (!!addsymbols){
+    characterset = characterset + getRandomSymbol();
+ }
+ 
+ //loop for character
+ var password = '';
+ for (let index = 0; index < passwordLength; index++) {
+    const randomCharacter = characterset[ Math.floor(Math.random() * characterset.length) ]
+     password = password + randomCharacter;
+ }
+
+ //var for text area to input password
+//add password text
+//add event lisenter for password button
+//passwordBttn.addEventListener('click', function(event){
 
 
 
+//});
+//if it has x run that function
